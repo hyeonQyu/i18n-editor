dbd8c1b759d4c5df64948ed0d537e70921429ac4
hyeonQyu <dhk0561@naver.com>
2022-07-07 09:40:36 +0900

    fix: ui 빌드 오류 수정

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    exportPathMap: () => ({
+        '/': { page: '/' },
+    }),
+    images: {
+        loader: 'akamai',
+        path: '/',
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
