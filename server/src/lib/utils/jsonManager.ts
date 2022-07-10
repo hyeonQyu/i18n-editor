759e21dce8fa87f4b2f8c65d9ba133663223694e
Hyeongyu Kim <dhk0561@naver.com>
2022-07-10 20:20:58 +0900

    fix: 저장 시 JSON 파일 포맷 오류 수정

--- server/src/lib/utils/jsonManager.ts
+++ server/src/lib/utils/jsonManager.ts
@@ -15,16 +15,14 @@ export namespace JsonManager {
             FileSystemManager.createDirectoryWhenNotExist(localeDirectoryPath);
 
             LANGUAGES.forEach((language) => {
-                console.log('언어: ', language);
-
                 const languageDirectoryPath = `${localeDirectoryPath}/${language}`;
                 FileSystemManager.createDirectoryWhenNotExist(languageDirectoryPath);
 
                 const filePath = `${languageDirectoryPath}/${name}`;
-                const json = language === 'ko' ? getKoreanJson(texts) : getNotKoreanJson(texts, filePath);
+                const json = JSON.parse(language === 'ko' ? getKoreanJson(texts) : getNotKoreanJson(texts, filePath));
 
-                console.log(json);
                 fs.writeFileSync(filePath, JSON.stringify(json, null, 4));
+                console.log(`${language}/${name}`, '생성 완료');
             });
             console.log('다국어 JSON 파일 생성이 완료되었습니다.');
         } catch (e) {
