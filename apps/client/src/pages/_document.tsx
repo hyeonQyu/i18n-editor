822cc118df57174162c2fff88c6ef513f549efe1
Hyeongyu Kim <dhk0561@naver.com>
2023-03-12 22:31:25 +0900

    chore: 파비콘 추가

---
+++ apps/client/src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
+          <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
