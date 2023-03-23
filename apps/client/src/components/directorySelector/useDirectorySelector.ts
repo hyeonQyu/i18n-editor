ebe24f83e3968fb6683f6a01cf3c2e3bb6f4e917
Hyeongyu Kim <dhk0561@naver.com>
2023-03-23 23:15:07 +0900

    feat: DirectorySelector 새 로직 추가

---
+++ apps/client/src/components/directorySelector/useDirectorySelector.ts
@@ -0,0 +1,110 @@
+import { useToastContext } from '@contexts/toastContext';
+import { FocusEventHandler, MouseEventHandler, RefObject, useRef, useState } from 'react';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { Menu } from 'primereact/menu';
+import useLocaleDirectoryPath from '@hooks/stores/useLocaleDirectoryPath';
+import useQueryGetNativeFileExplorer from '@hooks/queries/useQueryGetNativeFileExplorer';
+import { CustomEventHandler } from '@defines/event';
+import { MenuItem } from 'primereact/menuitem';
+import useQueryGetTranslationFile from '@hooks/queries/useQueryGetTranslationFile';
+import useLocaleDirectoryCreationDialogOpened from '@hooks/stores/useLocaleDirectoryCreationDialogOpened';
+
+export interface UseDirectorySelector {
+  fileExplorerRef: RefObject<OverlayPanel>;
+  menuRef: RefObject<Menu>;
+  isFileExplorerOpened: boolean;
+  menuItems: MenuItem[];
+  invalid: boolean;
+  handleOpenFileExplorerButtonClick: MouseEventHandler<HTMLButtonElement>;
+  handleInputFocus: FocusEventHandler<HTMLInputElement>;
+  handleMenuClick: MouseEventHandler<HTMLButtonElement>;
+  handleFileExplorerShow: CustomEventHandler;
+  handleFileExplorerHide: CustomEventHandler;
+}
+
+export default function useDirectorySelector(): UseDirectorySelector {
+  const { toastRef } = useToastContext();
+  const fileExplorerRef = useRef<OverlayPanel>(null);
+  const menuRef = useRef<Menu>(null);
+
+  const { localeDirectoryPath } = useLocaleDirectoryPath();
+  const { localeDirectoryCreationDialogOpened } = useLocaleDirectoryCreationDialogOpened();
+
+  const [isFileExplorerOpened, setIsFileExplorerOpened] = useState(false);
+
+  // OS 파일 탐색기에서 locale directory 열기
+  const { refetch: refetchGetFileExplorer } = useQueryGetNativeFileExplorer({
+    req: {
+      path: localeDirectoryPath || '',
+    },
+    queryOption: {
+      enabled: false,
+    },
+  });
+
+  // 번역 파일 목록 조회
+  const { error: errorGetTranslationFile } = useQueryGetTranslationFile({
+    req: {
+      path: localeDirectoryPath!,
+    },
+    queryOption: {
+      enabled: Boolean(localeDirectoryPath) && !localeDirectoryCreationDialogOpened,
+    },
+  });
+  const invalid = Boolean(errorGetTranslationFile);
+
+  // i18n-editor 파일 탐색기 열기 버튼 클릭
+  const handleOpenFileExplorerButtonClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    fileExplorerRef.current?.toggle(e);
+  };
+
+  const handleInputFocus: FocusEventHandler<HTMLInputElement> = (e) => {
+    e.target.blur();
+  };
+
+  const handleMenuClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    menuRef.current?.toggle(e);
+  };
+
+  const handleFileExplorerShow: CustomEventHandler = () => {
+    setIsFileExplorerOpened(true);
+  };
+
+  const handleFileExplorerHide: CustomEventHandler = () => {
+    setIsFileExplorerOpened(false);
+  };
+
+  const menuItems: MenuItem[] = [
+    {
+      label: '디렉토리 열기',
+      icon: 'pi pi-folder-open',
+      async command() {
+        await refetchGetFileExplorer();
+      },
+    },
+    {
+      label: '디렉토리 경로 복사',
+      icon: 'pi pi-clone',
+      async command() {
+        await navigator.clipboard.writeText(localeDirectoryPath!);
+        toastRef.current?.show({
+          severity: 'info',
+          detail: '클립보드에 복사되었어요',
+        });
+      },
+    },
+  ];
+
+  return {
+    fileExplorerRef,
+    menuRef,
+    isFileExplorerOpened,
+    menuItems,
+    invalid,
+    handleOpenFileExplorerButtonClick,
+    handleInputFocus,
+    handleMenuClick,
+    handleFileExplorerShow,
+    handleFileExplorerHide,
+  };
+}
