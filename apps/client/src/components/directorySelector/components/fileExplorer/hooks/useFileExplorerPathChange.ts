78bcc366103b27fc15e36ae5b4cbce8dce9f8769
Hyeongyu Kim <dhk0561@naver.com>
2023-03-30 00:45:45 +0900

    feat: fileExplorer 경로 이동 hook 추가

---
+++ apps/client/src/components/directorySelector/components/fileExplorer/hooks/useFileExplorerPathChange.ts
@@ -0,0 +1,51 @@
+import { useSetRecoilState } from 'recoil';
+import { fileExplorerStates } from '@components/directorySelector/components/fileExplorer/stores/store';
+
+export interface UseFileExplorerPathChange {
+  changePathForward: (path: string | ((prevPath: string) => string), clearForwardStack?: boolean) => void;
+  changePathBackward: (path: string | ((prevPath: string) => string)) => void;
+}
+
+export default function useFileExplorerPathChange(): UseFileExplorerPathChange {
+  const setPath = useSetRecoilState(fileExplorerStates.path);
+
+  const setForwardStack = useSetRecoilState(fileExplorerStates.forwardStack);
+  const setBackwardStack = useSetRecoilState(fileExplorerStates.backwardStack);
+
+  /**
+   * 앞으로 이동
+   * @param path 새 경로 혹은 새 경로를 반환하는 함수
+   * @param clearForwardStack forwardStack clear 여부
+   */
+  const changePathForward = (path: string | ((prevPath: string) => string), clearForwardStack?: boolean) => {
+    setPath((prev) => {
+      setBackwardStack((stack) => [...stack, prev]);
+
+      if (clearForwardStack) {
+        setForwardStack([]);
+      } else {
+        setForwardStack((stack) => stack.slice(0, stack.length - 1));
+      }
+
+      return typeof path === 'function' ? path(prev) : path;
+    });
+  };
+
+  /**
+   * 뒤로 이동
+   * @param path
+   */
+  const changePathBackward = (path: string | ((prevPath: string) => string)) => {
+    setPath((prev) => {
+      setForwardStack((stack) => [...stack, prev]);
+      setBackwardStack((stack) => stack.slice(0, stack.length - 1));
+
+      return typeof path === 'function' ? path(prev) : path;
+    });
+  };
+
+  return {
+    changePathForward,
+    changePathBackward,
+  };
+}
