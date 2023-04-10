e8dbdd05a76cf049994efdc385f90f1c80d84f16
Hyeongyu Kim <dhk0561@naver.com>
2023-04-10 23:04:52 +0900

    refactor: 기존 TranslationFileCreationDialog 컴포넌트 export 교체

--- apps/client/src/components/translationFileCreationDialog/index.ts
+++ apps/client/src/components/translationFileCreationDialog/index.ts
@@ -1,1 +1,5 @@
+// TODO 삭제
 export * from './TranslationFileCreationDialogBefore';
+
+export { default as TranslationFileCreationDialog } from './TranslationFileCreationDialog';
+export type { TranslationFileCreationDialogProps } from './TranslationFileCreationDialog';
