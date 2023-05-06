a4afdece8c6c89a47c2f88c31a1c51986a8f7158
Hyeongyu Kim <dhk0561@naver.com>
2023-05-06 22:25:28 +0900

    refactor: 신규 번역 추가 로직 리팩토링

--- apps/client/src/components/translationFileEditor/components/translationContentTable/components/newTranslationAdder/useNewTranslationAdder.ts
+++ apps/client/src/components/translationFileEditor/components/translationContentTable/components/newTranslationAdder/useNewTranslationAdder.ts
@@ -1,13 +1,9 @@
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { translationFileEditorStates } from '@components/translationFileEditor/stores/store';
 import { FormEventHandler } from 'react';
-import useMutationPostContentRow from '@hooks/queries/useMutationPostContentRow';
-import { localeDirectoryPathState, translationFileNameState } from '@stores/store';
 import useInput, { IUseInput } from '@hooks/common/useInput';
-import { getNewContentRow } from '@utils/tableUtil';
-import { useToastContext } from '@contexts/toastContext';
-import useInputFilter from '@components/translationFileEditor/hooks/useInputFilter';
 import { NewTranslationAdderProps } from '@components/translationFileEditor/components/translationContentTable/components/newTranslationAdder/NewTranslationAdder';
+import useAddRow from '@components/translationFileEditor/hooks/useAddRow';
 
 export interface UseNewTranslationAdderParams extends NewTranslationAdderProps {}
 
@@ -20,64 +16,18 @@ export interface UseNewTranslationAdder {
 export default function useNewTranslationAdder(params: UseNewTranslationAdderParams): UseNewTranslationAdder {
   const {} = params;
 
-  const { toastRef } = useToastContext();
-
-  const localeDirectoryPath = useRecoilValue(localeDirectoryPathState);
-  const translationFileName = useRecoilValue(translationFileNameState);
-
   const inputFilterValue = useRecoilValue(translationFileEditorStates.filterValue);
-  const [rows, setRows] = useRecoilState(translationFileEditorStates.rows);
-  const columns = useRecoilValue(translationFileEditorStates.columns);
-  const setEditRowIndex = useSetRecoilState(translationFileEditorStates.editRowIndex);
-
-  const inputFilter = useInputFilter({});
+  const rows = useRecoilValue(translationFileEditorStates.rows);
 
   const inputTranslationKey = useInput({});
 
   const isFiltered = Boolean(inputFilterValue && rows.length);
 
-  const { mutate: mutatePostContentRow } = useMutationPostContentRow({});
-
-  /**
-   * 새로운 행 추가
-   */
-  const addRow = () => {
-    mutatePostContentRow(
-      {
-        path: localeDirectoryPath!,
-        fileName: translationFileName!,
-        row: {
-          index: rows.length,
-          key: inputTranslationKey.value,
-        },
-      },
-      {
-        onSuccess({ data }) {
-          if (!data) return;
-          const {
-            row: { index, key },
-          } = data;
-
-          setRows((prev) => [...prev, getNewContentRow(columns, index, key)]);
-
-          toastRef.current?.show({
-            severity: 'success',
-            detail: '행을 추가했어요',
-            life: 3000,
-          });
-
-          setEditRowIndex(index);
-
-          if (key.toLowerCase().includes(inputFilterValue.toLowerCase())) return;
-          inputFilter.clear();
-        },
-      },
-    );
-  };
+  const { addRow } = useAddRow({});
 
   const handleFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    addRow();
+    addRow(inputTranslationKey.value);
   };
 
   return {
