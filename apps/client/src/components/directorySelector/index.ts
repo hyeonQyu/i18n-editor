908cec60c5fe95742e2f2e6a6cf9410f55768eb4
Hyeongyu Kim <dhk0561@naver.com>
2023-05-10 23:37:33 +0900

    refactor: DirectorySelector export 수정

--- apps/client/src/components/directorySelector/index.ts
+++ apps/client/src/components/directorySelector/index.ts
@@ -1,2 +1,5 @@
 // TODO 삭제
 export * from './DirectorySelectorBefore';
+
+export { default as DirectorySelector } from './DirectorySelector';
+export type { DirectorySelectorProps } from './DirectorySelector';
