6230194c3067fc62faa1f2bd3b6f52294414e549
Hyeongyu Kim <dhk0561@naver.com>
2023-05-13 23:43:54 +0900

    fix: 빌드 후 아이콘 깨짐 문제 해결

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -13,7 +13,8 @@ class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
-          <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+          <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />{' '}
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/primeicons@4.1.0/primeicons.css" />
         </Head>
         <body>
           <Main />
