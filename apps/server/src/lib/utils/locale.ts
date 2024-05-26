a54be6e688e4b0e4fd11a258a5749929e7402f56
Hyeongyu Kim <dhk0561@naver.com>
2024-05-26 19:37:50 +0900

    [IE-23] feat: namespace 번역 파일 조회 API

--- apps/server/src/lib/utils/locale.ts
+++ apps/server/src/lib/utils/locale.ts
@@ -1,15 +1,15 @@
 import fs from 'fs';
-import { LANGUAGE_CODE_SET } from 'i18n-editor-common';
+import { LanguageCode, LANGUAGE_CODE_SET } from 'i18n-editor-common';
 import { GLOBAL } from '../defines/global';
 
 const isLanguageDirectory = (dirent: fs.Dirent) => {
   return dirent.isDirectory() && LANGUAGE_CODE_SET.has(dirent.name);
 };
 
-export const getLanguages = async (localeDirectoryPath: string) => {
+export const getLanguageCodes = async (localeDirectoryPath: string) => {
   return (await fs.promises.readdir(localeDirectoryPath, { withFileTypes: true }))
     .filter(isLanguageDirectory)
-    .map((dirent) => dirent.name)
+    .map((dirent) => dirent.name as LanguageCode)
     .sort((a, b) => {
       if (a === GLOBAL.defaultLanguage) return -1;
       if (b === GLOBAL.defaultLanguage) return 1;
