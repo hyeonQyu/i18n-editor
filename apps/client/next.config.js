6ee69cf552a4ec910d93b1e0e56dcc16f4f332c7
hyeonQyu <dhk0561@naver.com>
2024-11-13 15:41:18 +0900

    [IE-66] 핫 리로드 문제 수정

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,19 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    loader: 'akamai',
-    path: '/',
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
 
-    return config;
-  },
-  assetPrefix: '.',
+module.exports = {
+  reactStrictMode: true,
 };
-
-module.exports = nextConfig;
