c95feeb74045252d138a842e1505e762ba99215c
hyeonQyu <dhk0561@naver.com>
2024-11-13 17:09:41 +0900

    [IE-66] notistack 패키지 설치

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,32 +1,28 @@
-import { Portal } from '../deprecated/components/portal';
-import { ToastContext } from '../deprecated/contexts/toastContext';
-import useApp from '../deprecated/hooks/pages/useApp';
-import { QueryClientProvider } from '@tanstack/react-query';
+// import { ToastContext } from '../deprecated/contexts/toastContext';
+// import useApp from '../deprecated/hooks/pages/useApp';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app';
-import 'primeicons/primeicons.css';
-import { ConfirmDialog } from 'primereact/confirmdialog';
-import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import { Toast } from 'primereact/toast';
 import { RecoilRoot } from 'recoil';
 import '../styles/animations.css';
 import '../styles/globals.css';
 import '../styles/reset.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { queryClient, toastRef } = useApp();
+  // const { queryClient, toastRef } = useApp();
 
   return (
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <Portal.Provider>
-          <ToastContext.Provider value={{ toastRef }}>
-            <Component {...pageProps} />
-            <Toast ref={toastRef} />
-            <ConfirmDialog />
-          </ToastContext.Provider>
-        </Portal.Provider>
-      </QueryClientProvider>
+      {/*<QueryClientProvider client={queryClient}>*/}
+      {/*<Portal.Provider>*/}
+      {/*<ToastContext.Provider value={{ toastRef }}>*/}
+      <Component {...pageProps} />
+      {/*<Toast ref={toastRef} />*/}
+      {/*</ToastContext.Provider>*/}
+      {/*</Portal.Provider>*/}
+      {/*</QueryClientProvider>*/}
     </RecoilRoot>
   );
 }
