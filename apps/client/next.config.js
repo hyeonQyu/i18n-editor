1883d35a6eb745f3cbab90adb7e0a5a6e0c4748a
hyeonQyu <dhk0561@naver.com>
2024-11-14 12:42:51 +0900

    [IE-69] lodash 트리 쉐이킹을 위한 빌드 시 import문 수정

--- apps/client/next.config.js
+++ apps/client/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    modularizeImports: {
+      lodash: {
+        transform: 'loadsh/{{member}}',
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
