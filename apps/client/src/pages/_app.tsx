953e94e5accd63215000f0fd0f60d6cfb3475068
hyeonQyu <dhk0561@naver.com>
2024-11-14 09:41:49 +0900

    [IE-67] roboto 폰트 추가

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,8 +1,9 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app';
 import { SnackbarProvider } from 'notistack';
-import 'primeicons/primeicons.css';
-import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import '../styles/animations.css';
 import '../styles/globals.css';
 import '../styles/reset.css';
