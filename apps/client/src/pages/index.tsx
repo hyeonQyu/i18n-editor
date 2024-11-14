71b68c6b196315d41778c267fa5af6d2b02b8e82
hyeonQyu <dhk0561@naver.com>
2024-11-14 10:11:51 +0900

    [IE-67] 폴더 경로를 표시하면서 복사 가능한 컴포넌트

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -1,145 +1,18 @@
-import { DirectorySelector } from '../deprecated/components/directorySelector';
-import { LocaleDirectoryCreationDialog } from '../deprecated/components/localeDirectoryCreationDialog';
-import { HomeSkeleton } from '../deprecated/components/page/home/homeSkeleton';
-import { TranslationFileCreationDialog } from '../deprecated/components/translationFileCreationDialog';
-import { TranslationFileEditor } from '../deprecated/components/translationFileEditor';
-import { TranslationFileSelector } from '../deprecated/components/translationFileSelector';
-import useHome from '../deprecated/hooks/pages/useHome';
-import Head from 'next/head';
+import DirectorySelector from '@components/DirectorySelector';
+import { Box } from '@mui/material';
 
-export interface IndexProps {}
-
-function Index(props: IndexProps) {
-  const {} = props;
-  const {
-    directoryPath,
-    translationFiles,
-    translationFile,
-    hasDirectorySelectorError,
-    contentColumns,
-    contentRows,
-    localeDirectoryCreationDialogOpened,
-    translationFileCreationDialogOpened,
-    isTranslationFileNameDuplicate,
-    isLoadingGetConfig,
-    inputNewTranslationFileName,
-    tableContainerRef,
-    handleDirectoryPathChange,
-    handleTranslationFileChange,
-    handleCreateLocaleDirectory,
-    handleCloseLocaleDirectoryCreationDialog,
-    handleTranslationContentChange,
-    handleAddTranslationFileButtonClick,
-    handleCreateTranslationFile,
-    handleCloseTranslationFileCreationDialog,
-    onAddColumn,
-    onDeleteColumn,
-    onAddRowAbove,
-    onAddRowBelow,
-    onAddRow,
-    onClearRowContent,
-    onDeleteRow,
-  } = useHome({});
-
+function HomePage() {
   return (
-    <>
-      <Head>
-        <title>i18n-editor</title>
-      </Head>
-
-      <LocaleDirectoryCreationDialog
-        visible={localeDirectoryCreationDialogOpened}
-        onCreate={handleCreateLocaleDirectory}
-        onHide={handleCloseLocaleDirectoryCreationDialog}
-      />
-      <TranslationFileCreationDialog
-        visible={translationFileCreationDialogOpened}
-        isDuplicate={isTranslationFileNameDuplicate}
-        inputTranslationFileName={inputNewTranslationFileName}
-        onCreate={handleCreateTranslationFile}
-        onHide={handleCloseTranslationFileCreationDialog}
-      />
-
-      {isLoadingGetConfig ? (
-        <HomeSkeleton />
-      ) : (
-        <>
-          <div className={'file-select-container'}>
-            <DirectorySelector path={directoryPath} invalid={hasDirectorySelectorError} onChange={handleDirectoryPathChange} />
-
-            <TranslationFileSelector
-              directoryPath={directoryPath}
-              file={translationFile}
-              files={translationFiles}
-              hasDirectorySelectorError={hasDirectorySelectorError}
-              onChange={handleTranslationFileChange}
-              onAddTranslationFileButtonClick={handleAddTranslationFileButtonClick}
-            />
-          </div>
-
-          <div className={'table-container'} ref={tableContainerRef}>
-            <TranslationFileEditor
-              columns={contentColumns}
-              rows={contentRows}
-              onChange={handleTranslationContentChange}
-              onAddColumn={onAddColumn}
-              onDeleteColumn={onDeleteColumn}
-              onAddRowAbove={onAddRowAbove}
-              onAddRowBelow={onAddRowBelow}
-              onAddRow={onAddRow}
-              onClearRowContent={onClearRowContent}
-              onDeleteRow={onDeleteRow}
-            />
-          </div>
-        </>
-      )}
-
-      <style jsx>{`
-        .file-select-container {
-          padding: 60px 0;
-          width: 600px;
-          margin: 0 auto;
-        }
-
-        .file-select-container > :global(*:not(:first-child)) {
-          margin-top: 42px;
-        }
-
-        .table-container {
-          width: 100%;
-          ${Boolean(contentColumns) && Boolean(contentRows) ? 'height: 100vh' : ''};
-          padding: 0 50px;
-        }
-        :global(.p-datatable-wrapper::-webkit-scrollbar),
-        :global(.p-dropdown-items-wrapper::-webkit-scrollbar),
-        :global(.p-multiselect-items-wrapper::-webkit-scrollbar) {
-          width: 8px;
-          background-color: var(--surface-b);
-        }
-        :global(.p-datatable-wrapper::-webkit-scrollbar-thumb),
-        :global(.p-dropdown-items-wrapper::-webkit-scrollbar-thumb),
-        :global(.p-multiselect-items-wrapper::-webkit-scrollbar-thumb) {
-          background-color: var(--surface-400);
-          border-radius: 10px;
-        }
-        :global(.p-datatable-wrapper::-webkit-scrollbar-thumb:hover),
-        :global(.p-dropdown-items-wrapper::-webkit-scrollbar-thumb:hover),
-        :global(.p-multiselect-items-wrapper::-webkit-scrollbar-thumb:hover) {
-          background-color: var(--surface-500);
-        }
-        :global(.p-datatable-wrapper::-webkit-scrollbar-thumb:active),
-        :global(.p-dropdown-items-wrapper::-webkit-scrollbar-thumb:active),
-        :global(.p-multiselect-items-wrapper::-webkit-scrollbar-thumb:active) {
-          background-color: var(--surface-600);
-        }
-        :global(.p-datatable-wrapper::-webkit-scrollbar-track),
-        :global(.p-dropdown-items-wrapper::-webkit-scrollbar-track),
-        :global(.p-multiselect-items-wrapper::-webkit-scrollbar-track) {
-          background: none;
-        }
-      `}</style>
-    </>
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: '800px',
+        margin: '0 auto',
+      }}
+    >
+      <DirectorySelector />
+    </Box>
   );
 }
 
-export default Index;
+export default HomePage;
