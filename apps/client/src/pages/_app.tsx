9cc5965789c3e9baba97971a7344f06b5e83bc44
hyeonQyu <dhk0561@naver.com>
2024-11-14 14:14:46 +0900

    [IE-69] AxiosProvider로 axios instance 주입

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import AxiosProvider from '@providers/AxiosProvider';
 import type { AppProps } from 'next/app';
 import { SnackbarProvider } from 'notistack';
 import '../styles/animations.css';
@@ -11,7 +12,9 @@ import '../styles/reset.css';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SnackbarProvider>
-      <Component {...pageProps} />
+      <AxiosProvider>
+        <Component {...pageProps} />
+      </AxiosProvider>
     </SnackbarProvider>
   );
 }
--- packages/app-common/src/defines/config.ts
+++ packages/app-common/src/defines/config.ts
@@ -5,3 +5,11 @@ export interface EditorConfig {
 export interface ConfigMeta {
   path: string;
 }
+
+export interface AppConfig {
+  port: number;
+}
+
+export const DEFAULT_APP_CONFIG: AppConfig = {
+  port: 4848,
+};
