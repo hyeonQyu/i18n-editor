013584bfe337e1e2085ec4e0acdf82ce2565e298
hyeonQyu <dhk0561@naver.com>
2024-11-15 18:19:01 +0900

    [IE-72] 외부 클릭 시 파일 관리자 검색창 닫기

--- apps/client/src/components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/index.tsx
+++ apps/client/src/components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/index.tsx
@@ -1,12 +1,15 @@
 import FileManagerExitSearchIconButton from '@components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/components/FileManagerExitSearchIconButton';
 import FileManagerSearchInput from '@components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/components/FileManagerSearchInput';
 import { useFileManagerSearchStore } from '@components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/stores/fileManagerSearch';
+import useClickOutside from '@hooks/useClickOutside';
 import { Box } from '@mui/material';
 import FileManagerSearchIconButton from 'components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/components/FileManagerSearchIconButton';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 function FileManagerSearch() {
   const { searchMode, reset } = useFileManagerSearchStore();
+
+  const ref = useRef<HTMLElement>();
 
   useEffect(() => {
     return () => {
@@ -14,8 +17,13 @@ function FileManagerSearch() {
     };
   }, []);
 
+  useClickOutside(ref, () => {
+    reset();
+  });
+
   return (
     <Box
+      ref={ref}
       sx={{
         display: 'flex',
         justifyContent: 'end',
