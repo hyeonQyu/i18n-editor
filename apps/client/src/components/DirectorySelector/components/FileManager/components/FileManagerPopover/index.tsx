b2bd33ace5a414e93792815dfdce4618609e0671
hyeonQyu <dhk0561@naver.com>
2024-11-15 12:22:54 +0900

    [IE-72] popover 역할을 담당하는 FileManagerPopover 컴포넌트 분리

---
+++ apps/client/src/components/DirectorySelector/components/FileManager/components/FileManagerPopover/index.tsx
@@ -0,0 +1,51 @@
+import { FILE_MANAGER_ID } from '@components/DirectorySelector/defines/attributes';
+import { useFileManagerStore } from '@components/DirectorySelector/stores/fileManagerStore';
+import { SELECTOR_MAX_WIDTH } from '@defines/styles';
+import { Box, Popover } from '@mui/material';
+import { ReactNode } from 'react';
+
+interface FileManagerPopoverProps {
+  children: ReactNode;
+}
+
+function FileManagerPopover(props: FileManagerPopoverProps) {
+  const { children } = props;
+
+  const { anchorElement, close } = useFileManagerStore();
+
+  const opened = Boolean(anchorElement);
+
+  return (
+    <Popover
+      id={FILE_MANAGER_ID}
+      open={opened}
+      anchorEl={anchorElement}
+      onClose={close}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'left',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'left',
+      }}
+      sx={{
+        '& .MuiPopover-paper': {
+          width: `${SELECTOR_MAX_WIDTH}px`,
+          height: '500px',
+          transform: 'translateY(12px) !important',
+        },
+      }}
+    >
+      <Box
+        sx={{
+          padding: '20px',
+        }}
+      >
+        {children}
+      </Box>
+    </Popover>
+  );
+}
+
+export default FileManagerPopover;
