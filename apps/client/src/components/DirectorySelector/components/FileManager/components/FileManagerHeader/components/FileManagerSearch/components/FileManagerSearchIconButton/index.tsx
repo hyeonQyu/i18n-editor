b3b431e470460d67ac13b9a7bd8766a814b073c7
hyeonQyu <dhk0561@naver.com>
2024-11-25 11:14:19 +0900

    [IE-89] cmd, ctrl 키 label 출력

--- apps/client/src/components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/components/FileManagerSearchIconButton/index.tsx
+++ apps/client/src/components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/components/FileManagerSearchIconButton/index.tsx
@@ -1,28 +1,17 @@
 import { FILE_MANAGER_SEARCH_TRANSITION_SECONDS } from '@components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/defines/animation';
 import useFileManagerSearchInputWidth from '@components/DirectorySelector/components/FileManager/components/FileManagerHeader/components/FileManagerSearch/hooks/useFileManagerSearchInputWidth';
 import { useFileManagerStore } from '@components/DirectorySelector/stores/fileManagerStore';
-import useOS from '@hooks/useOS';
+import useMetaKeyLabel from '@hooks/useMetaKeyLabel';
 import SearchIcon from '@mui/icons-material/Search';
 import { IconButton, Tooltip } from '@mui/material';
 
-const getShortcutByOS = (os: string | undefined) => {
-  switch (os) {
-    case 'macos':
-      return '⌘ + F';
-    case 'win':
-    case 'linux':
-    default:
-      return 'Ctrl + F';
-  }
-};
-
 function FileManagerSearchIconButton() {
-  const os = useOS();
   const { searchMode, startSearch: handleClick } = useFileManagerStore();
   const right = useFileManagerSearchInputWidth();
+  const metaKey = useMetaKeyLabel();
 
   return (
-    <Tooltip title={searchMode ? undefined : `검색창 열기 (${getShortcutByOS(os)})`}>
+    <Tooltip title={searchMode ? undefined : `검색창 열기 (${metaKey} + F)`}>
       <IconButton
         aria-label={'search file manager'}
         onClick={handleClick}
