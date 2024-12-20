8bb39c7c1a350a2788d471aa3162a6e8f931fe26
hyeonQyu <dhk0561@naver.com>
2024-12-20 17:59:31 +0900

    [IE-125] 경로 복사 버튼 위치 이동

--- apps/client/src/components/WorkspaceView/components/WorkspaceViewHeader/components/WorkspaceMenu/index.tsx
+++ apps/client/src/components/WorkspaceView/components/WorkspaceViewHeader/components/WorkspaceMenu/index.tsx
@@ -1,13 +1,11 @@
 import DirectoryChangeButton from '@components/WorkspaceView/components/WorkspaceViewHeader/components/WorkspaceMenu/components/DirectoryChangeButton';
 import NativeFileManagerOpenButton from '@components/WorkspaceView/components/WorkspaceViewHeader/components/WorkspaceMenu/components/NativeFileManagerOpenButton';
 import { Box } from '@mui/material';
-import DirectoryPathCopyButton from 'components/WorkspaceView/components/WorkspaceViewHeader/components/WorkspaceMenu/components/DirectoryPathCopyButton';
 
 function WorkspaceMenu() {
   return (
     <Box sx={{ display: 'flex', gap: '8px', minWidth: 'fit-content' }}>
       <DirectoryChangeButton />
-      <DirectoryPathCopyButton />
       <NativeFileManagerOpenButton />
     </Box>
   );
