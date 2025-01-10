0dd99361734ca318d65c0fccc7994036b3a082cc
hyeonQyu <dhk0561@naver.com>
2025-01-10 15:55:03 +0900

    [IE-145] 번역 추가 시 중복 검사

--- apps/server/src/lib/apis/services/NamespaceService/getNamespace.ts
+++ apps/server/src/lib/apis/services/NamespaceService/getNamespace.ts
@@ -1,4 +1,5 @@
 import { type GetNamespaceResponse } from 'i18n-editor-common';
+import { namespaceContainer } from '../../../utils/namespaceContainer';
 import { getLanguageCodesByWorkspacePath, getWorkspacePath, readTranslations } from './common/utils';
 
 export const getNamespace: (workspaceId: string, namespace: string) => Promise<GetNamespaceResponse> = async (workspaceId, namespace) => {
@@ -6,6 +7,8 @@ export const getNamespace: (workspaceId: string, namespace: string) => Promise<G
 
   const languageCodes = await getLanguageCodesByWorkspacePath(path);
   const translations = await readTranslations(path, namespace, languageCodes);
+
+  namespaceContainer.setTranslations(workspaceId, namespace, translations);
 
   return {
     languageCodes,
