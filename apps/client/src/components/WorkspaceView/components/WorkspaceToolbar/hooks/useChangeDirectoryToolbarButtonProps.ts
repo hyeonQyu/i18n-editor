014fa5673a914da26c27f1b9b1ba6eec09cbf685
hyeonQyu <dhk0561@naver.com>
2025-01-29 21:05:18 +0900

    [IE-159] 폴더 변경 툴바 아이콘 변경

--- apps/client/src/components/WorkspaceView/components/WorkspaceToolbar/hooks/useChangeDirectoryToolbarButtonProps.ts
+++ apps/client/src/components/WorkspaceView/components/WorkspaceToolbar/hooks/useChangeDirectoryToolbarButtonProps.ts
@@ -2,7 +2,7 @@ import { WorkspaceToolbarButtonProps } from '@components/WorkspaceView/component
 import useOpenFileManagerDialog from '@hooks/useOpenFileManagerDialog';
 import useUpdateWorkspace from '@hooks/workspace/useUpdateWorkspace';
 import useWorkspace from '@hooks/workspace/useWorkspace';
-import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import FolderCopyOutlinedIcon from '@mui/icons-material/FolderCopyOutlined';
 
 function useChangeDirectoryToolbarButtonProps(): WorkspaceToolbarButtonProps {
   const workspace = useWorkspace();
@@ -12,7 +12,7 @@ function useChangeDirectoryToolbarButtonProps(): WorkspaceToolbarButtonProps {
   const updateWorkspace = useUpdateWorkspace();
 
   return {
-    IconComponent: FolderOpenIcon,
+    IconComponent: FolderCopyOutlinedIcon,
     label: '디렉토리 변경',
     onClick: () => {
       if (!workspace?.path) return;
