3a9fae6339021e57feaf927df18ba9e038760262
Hyeongyu Kim <dhk0561@naver.com>
2025-01-30 20:37:14 +0900

    [IE-35] 클라이언트 deprecated 코드 제거

--- apps/client/src/deprecated/apis/config/index.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosRequestFunction } from '@defines/api';
-import BaseAPI from '@utils/BaseAPI';
-import { GetConfigRequest, GetConfigResponse } from 'i18n-editor-common';
-
-export default class ConfigApi extends BaseAPI {
-  public getConfig: AxiosRequestFunction<GetConfigRequest, GetConfigResponse> = () => {
-    return this.axiosInstance.get(this.getUrl('/config'));
-  };
-}
