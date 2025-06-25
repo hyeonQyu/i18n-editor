32d6097dcc168438fd8131be6ab06ec3e628a678
hyeonQyu <dhk0561@naver.com>
2025-06-25 16:48:00 +0900

    [IE-229] Modularize window creation logic as a utility

---
+++ packages/electron/src/utils/window.utils.ts
@@ -0,0 +1,48 @@
+import { app, BrowserWindow } from 'electron';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { getEnvironment } from './env.utils';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+let mainWindow: BrowserWindow | null = null;
+
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
+
+export const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      sandbox: false,
+      preload: join(__dirname, 'preload.js'),
+    },
+  });
+
+  const isDev = getEnvironment() === 'development';
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
