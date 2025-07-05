d0ad49a1508a6366fe20c915ebaaf704c7854a45
hyeonQyu <dhk0561@naver.com>
2025-07-05 18:42:20 +0900

    [IE-232] Remove unnecessary hooks

--- packages/renderer/src/hooks/domains/ui/useUIConfig.ts
+++ packages/renderer/src/hooks/domains/ui/useUIConfig.ts
@@ -1,8 +1,14 @@
-import { useQueryReadUIConfig } from '@/hooks/domains/ui/useQueryReadUIConfig';
+import { QUERY_KEY } from '@/constants/reactQuery.query.constants';
+import { useElectronAPI } from '@/hooks/common/useElectronAPI';
 import { DEFAULT_CONFIG } from '@i18n-editor/shared';
+import { useQuery } from '@tanstack/react-query';
 
 export const useUIConfig = () => {
-  const { data: ui = DEFAULT_CONFIG.ui } = useQueryReadUIConfig({
+  const electronAPI = useElectronAPI();
+
+  const { data: ui = DEFAULT_CONFIG.ui } = useQuery({
+    queryKey: QUERY_KEY.config.ui.read(),
+    queryFn: () => electronAPI.config.ui.read(),
     staleTime: Infinity,
   });
 
