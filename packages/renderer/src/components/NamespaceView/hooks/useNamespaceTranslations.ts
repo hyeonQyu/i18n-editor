463ae2f309d810a76dc35308ad650aac5c696cb6
hyeonQyu <dhk0561@naver.com>
2025-09-01 16:20:53 +0900

    [IE-246] Implement search-based filtering for translation list

--- packages/renderer/src/components/NamespaceView/hooks/useNamespaceTranslations.ts
+++ packages/renderer/src/components/NamespaceView/hooks/useNamespaceTranslations.ts
@@ -1,10 +1,46 @@
+import { useNamespaceViewSearchStore } from '@/components/NamespaceView/stores/namespaceView.search.store';
 import { useNamespace } from '@/hooks/domains/namespace';
 import { useTranslations } from '@/hooks/domains/translation';
 import { useWorkspaceId } from '@/hooks/domains/workspace';
+import { TIME_UNIT, Translation } from '@i18n-editor/shared';
+import { throttle } from 'lodash-es';
+import { useEffect, useMemo, useState } from 'react';
 
 export const useNamespaceTranslations = () => {
   const workspaceId = useWorkspaceId();
   const namespace = useNamespace();
 
-  return useTranslations(workspaceId, namespace);
+  const allTranslations = useTranslations(workspaceId, namespace);
+  const [filteredTranslations, setFilteredTranslations] = useState<Translation[]>(allTranslations);
+
+  const keyword = useNamespaceViewSearchStore((store) => store.keyword);
+
+  const throttledSetFilteredTranslations = useMemo(
+    () =>
+      throttle((translations: Translation[], keyword: string) => {
+        const keywordLowerCase = keyword.toLowerCase();
+
+        setFilteredTranslations(
+          translations.filter((translation) => {
+            const keyMatch = translation.key.toLowerCase().includes(keywordLowerCase);
+            const valueMatch = Object.values(translation.value).some((value) => value.toLowerCase().includes(keywordLowerCase));
+
+            return keyMatch || valueMatch;
+          }),
+        );
+      }, TIME_UNIT.unitOfMs.asSecond * 0.5),
+    [],
+  );
+
+  useEffect(() => {
+    throttledSetFilteredTranslations(allTranslations, keyword);
+  }, [keyword, allTranslations]);
+
+  useEffect(() => {
+    return () => {
+      throttledSetFilteredTranslations.cancel();
+    };
+  }, [throttledSetFilteredTranslations]);
+
+  return filteredTranslations;
 };
